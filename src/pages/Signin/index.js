926702a63f1af64739cd3e0401a212bee5c10fbd
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-06 20:38:12 -0300

    adiciona estilos do login e cadastro

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 function Signin() {
-    return <div>signin</div>;
+    return (
+        <>
+            <img src={logo} alt="Logo" />
+
+            <form action="">
+                <input type="email" placeholder="Seu e-mail" />
+                <input type="password" placeholder="Sua senha secreta" />
+
+                <button type="submit">Acessar</button>
+                <Link to="/signup">Criar conta gratuita</Link>
+            </form>
+        </>
+    );
 }
 
 export default Signin;
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 function Signup() {
-    return <div>signup</div>;
+    return (
+        <>
+            <img src={logo} alt="Logo" />
+
+            <form action="">
+                <input placeholder="Nome completo" />
+                <input type="email" placeholder="Seu e-mail" />
+                <input type="password" placeholder="Sua senha secreta" />
+
+                <button type="submit">Criar conta</button>
+                <Link to="/">Já tenho login</Link>
+            </form>
+        </>
+    );
 }
 
 export default Signup;
