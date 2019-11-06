aa197ca5b30886f8d8d840c4fe0c096e9c2dce90
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-06 20:46:32 -0300

    adiciona lib para forms

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 function Signin() {
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
     return (
         <>
             <img src={logo} alt="Logo" />
 
-            <form action="">
-                <input type="email" placeholder="Seu e-mail" />
-                <input type="password" placeholder="Sua senha secreta" />
+            <Form onSubmit={handleSubmit}>
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha secreta"
+                />
 
                 <button type="submit">Acessar</button>
                 <Link to="/signup">Criar conta gratuita</Link>
-            </form>
+            </Form>
         </>
     );
 }
