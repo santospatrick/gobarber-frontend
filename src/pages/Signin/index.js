9a007b3089db470ea906e5e3a6616edb57a5056a
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-06 20:51:28 -0300

    adiciona validacoes nos formularios

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string().required('O senha é obrigatória')
+});
 
 function Signin() {
     function handleSubmit(data) {
         console.tron.log(data);
     }
+
     return (
         <>
             <img src={logo} alt="Logo" />
 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} schema={schema}>
                 <Input name="email" type="email" placeholder="Seu e-mail" />
                 <Input
                     name="password"
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string()
+        .min(6, 'No mínimo 6 caracteres')
+        .required('O senha é obrigatória')
+});
 
 function Signup() {
     function handleSubmit(data) {
@@ -11,7 +22,7 @@ function Signup() {
         <>
             <img src={logo} alt="Logo" />
 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} schema={schema} noValidate>
                 <Input name="name" placeholder="Nome completo" />
                 <Input name="email" type="email" placeholder="Seu e-mail" />
                 <Input
