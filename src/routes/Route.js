ddcbc483c0a0a6c307e1e1b8a7ebce56dd28b534
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-05 21:18:55 -0300

    adiciona styled-components

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
 
 function RouteWrapper({ component: Component, isPrivate = false, ...rest }) {
     const signed = false;
@@ -13,7 +15,18 @@ function RouteWrapper({ component: Component, isPrivate = false, ...rest }) {
         return <Redirect to="/dashboard" />;
     }
 
-    return <Route {...rest} component={Component} />;
+    const Layout = signed ? DefaultLayout : AuthLayout;
+
+    return (
+        <Route
+            {...rest}
+            render={props => (
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            )}
+        />
+    );
 }
 
 RouteWrapper.propTypes = {
