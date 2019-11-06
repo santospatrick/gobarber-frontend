7ca8d075765dcbb5c0ced76e821086e92ea70517
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-06 20:29:36 -0300

    adiciona root import

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
-import Profile from '../pages/Profile';
-import Dashboard from '../pages/Dashboard';
+import Signin from '~/pages/Signin';
+import Signup from '~/pages/Signup';
+import Profile from '~/pages/Profile';
+import Dashboard from '~/pages/Dashboard';
 
 export default function Routes() {
     return (
