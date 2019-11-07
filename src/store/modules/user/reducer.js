4bfa66e9cc0d2d7aea797251bae028b73c37964c
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-06 21:46:22 -0300

    adiciona reducer para usuario

---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,17 @@
+import produce from 'immer';
+
+const initialState = {
+    profile: null
+};
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        case '@auth/SIGN_IN_SUCCESS':
+            return produce(state, draft => {
+                draft.profile = payload.user;
+            });
+
+        default:
+            return state;
+    }
+};
