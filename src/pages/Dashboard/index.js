8f241028226142c85a5aa3539d95cda8c15c069b
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-08 22:47:45 -0300

    adiciona listagem de horarios disponiveis

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Time } from './styles';
 
 function Dashboard() {
-    api.get('appointments');
-    return <div>Dashboard</div>;
+    return (
+        <Container>
+            <header>
+                <button type="button">
+                    <MdChevronLeft size={36} color="#fff" />
+                </button>
+                <strong>31 de Maio</strong>
+                <button type="button">
+                    <MdChevronRight size={36} color="#fff" />
+                </button>
+            </header>
+
+            <ul>
+                <Time past>
+                    <strong>08:00</strong>
+                    <span>Diego Fernandes</span>
+                </Time>
+                <Time available>
+                    <strong>09:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong>10:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+                <Time>
+                    <strong>11:00</strong>
+                    <span>Em aberto</span>
+                </Time>
+            </ul>
+        </Container>
+    );
 }
 
 export default Dashboard;
